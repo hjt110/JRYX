f3cc1998b88c6034e9013e196f374732f3088d30
hjt110 <412700845@qq.com>
2018-06-05 22:22:27 +0800

    first commit

---
+++ app/src/main/java/com/wangou/jinriyixing/MainActivity.java
@@ -0,0 +1,26 @@
+package com.wangou.jinriyixing;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.tong.library.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.tv_main)
+    TextView tvMain;
+
+    @Override
+    protected int getLayoutResID() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void init(Bundle savedInstanceState) {
+
+    }
+
+}
